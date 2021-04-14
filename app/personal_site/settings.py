73f7fa9406e2b45c631ce82d3ff8ce5108e288b0
Ernie Massei <Emassei@users.noreply.github.com>
2021-04-14 15:59:01 -0500

    add ipython and admin for projects

--- app/personal_site/settings.py
+++ app/personal_site/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'projects',
-    'blog'
+    'blog',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
--- app/projects/models.py
+++ app/projects/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from django.db import models
-
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
